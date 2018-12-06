853a454a11b6abb191fd183f9143ce5de48394c0
archishadatta <archisha.datta@gmail.com>
2018-12-06 09:42:06 -0800

    added script for database create and music table

--- table.sql
+++ table.sql
@@ -1,43 +1,55 @@
 create database archiesdb;
 use archiesdb;
 create table music (
-	genre varchar (50),
-	artists varchar(100),
-    songs varchar(100),
-    origin varchar (100),
+    song varchar(100),
+    artist varchar(100),
+    genre varchar (50),
     characteristics varchar(100)
     );
-    
-insert into archiesdb
-values ('pop', 
-		'Taylor Swift, Justin Bieber, Ariana Grande',
-        '"Bye, Bye, Bye", "Oops I Did it Again", "You Dont Know Youre Beautiful"',
-        'derived from disco music in America from 1950s to 60s',
-        'catchy, simple, energetic, danceable, melodic'
-	);
+
+insert into music(
+songs,artists,genre,characteristics)
+values ('Baby',
+         'Justin Beiber',
+         'pop',
+         'one of the most disliked videos on YouTube');
         
-insert into archiesdb
-values ('classical/instrumental', 
-		'Tchaikovsky, Mozart, Beethoven, Bach',
-        '"Claire de Lune", "Fur Elise", "Blue Danube"',
-        'instrumental music has been around for almost 70,000 years',
-        'variety in dynamics, emphasis on emotion and beauty'
+insert into music(
+songs,artists,genre,characteristics)
+values
+        ('Baby',
+         'Justin Beiber',
+         'pop',
+         'one of the most disliked videos on YouTube'),
+        ('Oops I Did it Again',
+        'Britney Spears',
+        'pop',
+        'iconic 200s pop music'),
+		('Aveu Passione',
+        'Tchaikofsky',
+        'Classical',
+        'characteristic vibrato and chord progressions of the time'),
+        ('Prelude of Suite in G',
+        'Bach',
+        'Classical',
+        'one of the most famous cello suites'),
+        ('Fur Elise',
+        'Beethoven',
+        'Classical',
+        'commonly known piano piece'),
+        ('What a Wonderful World', 
+		'Louis Armstrong',
+        'jazz',
+        'syncopation and swing, scat singing, walking bass'),
+		('Hotel California',
+        'The Eagles',
+        'rock',
+        'heavy guitar, well-known song'),
+        ('Welcome to the Black Parade',
+        'My Chemical Romance',
+        'alternative',
+        'first notes are easily recognizable'
 	);
-
-insert into archiesdb
-values ('jazz', 
-		'Louis Armstrong, Dizzy Gillepsie, Ella Fitzgerald',
-        '"What a Wonderful World", "A Night in Tunisia", "How High the Moon"',
-        'comes from African American communities in New Orleans in the early 20th century',
-        'syncopation and swing, scat singing, walking bass'
-	);
-
-insert into archiesdb
-values ('hip-hop', 
-		'Taylor Swift, Justin Bieber, Ariana Grande',
-        'I dont know any hip hop songs',
-        'developed in Bronx in the late 1970s',
-        'synthesized beats and sounds, rhythmic beatboxing, rap'
-	);
+    
 
 
